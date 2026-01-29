02f08267eda8568e20f0bb3acdad74ac5b7b216c
Oscar Toribio <oscartoribio@Oscars-MacBook-Pro-M5.local>
2026-01-28 23:28:06 -0400

    implement user search and add go home button

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet, useNavigate } from 'react-router-dom'
 import './Layout.css'
+import { PATHS_DYNAMIC } from './app-routes';
 
 const Layout = () => {
   const navigate = useNavigate();
   return (
     <>
       <h1 onClick={() => navigate('/')}>User Intelligence Platform</h1>
+      <button className="go-home-btn" onClick={() => navigate(PATHS_DYNAMIC.HOME)}>Go Home</button>
       <main className='main-container'>
         <Outlet />
       </main>
